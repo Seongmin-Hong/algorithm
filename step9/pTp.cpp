49e4590857ba8b38aa31489f56188e8de4d9ac13
Hong SeongMin <43630677+Seongmin-Hong@users.noreply.github.com>
2018-11-04 17:52:25 +0900

    Update pTp.cpp

--- step9/pTp.cpp
+++ step9/pTp.cpp
@@ -1,17 +1,20 @@
-#include<stdio.h>
 
 void pTp(int x) {
-	if (x != 1) {			
-			pTp(x - 1);
-			printf("%d", x-1);
-			pTp(x - 1);
-	}
-	
+    if (x != 1) {
+        printf("%", x-1);
+        if (x != 1) {
+            pTp(x-1);
+            printf("%d", x-1);
+        }
+        pTp(x - 1);
+    }
+     
+     
 }
-
+ 
 int main() {
-	int n;
-	scanf("%d", &n);
-
-	pTp(n);
+    int n;
+    scanf("%d", &n);
+ 
+    pTp(n+1);
 }
